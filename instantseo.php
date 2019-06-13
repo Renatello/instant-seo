fd2e2e06b02557a3f475672a4c9650fc31b5c766
Renat Galyamov <renatello1@gmail.com>
2019-06-13 10:23:17 +0600

    initial commit

---
+++ instantseo.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: Instant Performance & SEO
+Plugin URI: https://renatello.com/instant-performance-seo/
+Description: Improve your site SEO and performance.
+Author: Renat Galyamov
+Version: 1.0
+Author URI: https://renatello.com/
+*/
+
+add_action( 'wp_enqueue_scripts', 'instantpage_wp_enqueue_scripts' );
+add_filter( 'script_loader_tag', 'instantpage_script_loader_tag', 10, 2 );
+
+function instantpage_wp_enqueue_scripts() {
+  wp_enqueue_script( 'instantpage', plugin_dir_url( __FILE__ ) . 'js/instantpage-1.2.2.js' );
+}
+
+function instantpage_script_loader_tag( $tag, $handle ) {
+  if ( 'instantpage' === $handle ) {
+    $tag = str_replace( 'text/javascript', 'module', $tag );
+  }
+  return $tag;
+}
---
+++ readme.txt
@@ -0,0 +1,15 @@
+﻿=== Instant Performance & SEO ===
+Contributors: renatello
+Tags: instant, performance, seo
+Requires at least: 4.2
+Tested up to: 5.2
+Stable tag: 1.2.2.1
+Requires PHP: 5.2.4
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl.html
+
+Improve your site SEO and performance.
+
+== Description ==
+
+Improve your site SEO and performance. More info on [https://renatello.com/instant-performance-seo/](https://renatello.com/instant-performance-seo/).
